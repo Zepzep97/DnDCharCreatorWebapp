0f3e10cbfc1446847b86b01097454362d0770cba
Steven Samrow <steven.samrow@gmail.com>
2019-10-13 23:18:31 -0500

    DB Models
    Altered DB models as well as added a DBcontext and a few other changes nessicary for models

---
+++ CharacterCreator/CharacterCreator/Migrations/20191014041301_Initial.cs
@@ -0,0 +1,94 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace CharacterCreator.Migrations
+{
+    public partial class Initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Class",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    Name = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Class", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Race",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    Name = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Race", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Features",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    Name = table.Column<string>(nullable: true),
+                    Sex = table.Column<string>(nullable: true),
+                    Alignment = table.Column<string>(nullable: true),
+                    Strength = table.Column<int>(nullable: false),
+                    Dexterity = table.Column<int>(nullable: false),
+                    Constitution = table.Column<int>(nullable: false),
+                    Inteligence = table.Column<int>(nullable: false),
+                    Wisdom = table.Column<int>(nullable: false),
+                    Charisma = table.Column<int>(nullable: false),
+                    ClassId = table.Column<int>(nullable: true),
+                    RaceId = table.Column<int>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Features", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Features_Class_ClassId",
+                        column: x => x.ClassId,
+                        principalTable: "Class",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK_Features_Race_RaceId",
+                        column: x => x.RaceId,
+                        principalTable: "Race",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Features_ClassId",
+                table: "Features",
+                column: "ClassId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Features_RaceId",
+                table: "Features",
+                column: "RaceId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Features");
+
+            migrationBuilder.DropTable(
+                name: "Class");
+
+            migrationBuilder.DropTable(
+                name: "Race");
+        }
+    }
+}
---
+++ CharacterCreator/CharacterCreator/Models/Characters.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CharacterCreator.Models
+{
+    public enum Sex
+    {
+        Male,
+        Female
+    }
+
+    public enum Alignment
+    {
+        LawfulGood,
+        NeutralGood,
+        ChaoticGood,
+        LawfulNeutral,
+        TrueNeutral,
+        ChaoticNeutral,
+        LawfulEvil,
+        NeutralEvil,
+        ChaoticEvil
+    }
+    public class Characters
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Sex { get; set; }
+        public string Alignment { get; set; }
+        public int Strength { get; set; }
+        public int Dexterity { get; set; }
+        public int Constitution { get; set; }
+        public int Inteligence { get; set; }
+        public int Wisdom { get; set; }
+        public int Charisma { get; set; }
+
+        public Class Class { get; set; }
+        public Race Race { get; set; }
+    }
+}
